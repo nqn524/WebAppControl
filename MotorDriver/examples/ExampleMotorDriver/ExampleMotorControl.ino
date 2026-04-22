c3c19c171aba844658b6c4a48a4c58f8687e7350
SekySmirfBoi <kickbottomarlito2@gmail.com>
2026-04-22 14:36:03 +0100

    awreklhugtsesoihy

--- MotorDriver/examples/ExampleMotorDriver/ExampleMotorControl.ino
+++ MotorDriver/examples/ExampleMotorDriver/ExampleMotorControl.ino
@@ -1,45 +1,12 @@
 #include "mbed.h"
 
 #include <RCControl.h>
-
-using namespace mbed;
+#include <MotorDriver.h>
 
 RCControl RCC("12345678-1234-1234-1234-123456789abc", "abcdef01-1234-1234-1234-123456789abc", "libTest");
+MotorDriver driver;
 
-/////////////////////////////////////////////////////////////
-
-DigitalOut leftMotorDir(P0_4);
-PwmOut     leftMotorPwm(P0_27);
-
-DigitalOut rightMotorDir(P0_5);
-PwmOut     rightMotorPwm(P1_2);
-
-InterruptIn leftEncoder(P1_11);
-InterruptIn rightEncoder(P1_12);
-
-volatile int leftEncoderCount = 0;
-volatile int rightEncoderCount = 0;
-
-/////////////////////////////////////////////////////////////
-
-
-
-float speedLeft = 0;
-float speedRight = 0;
-
-void setup() {
-
-  // Set PWM frequency
-  leftMotorPwm.period_us(2000);
-  rightMotorPwm.period_us(2000);
-
-  stopMotors();
-
-  // Encoder interrupts
-  leftEncoder.rise(&onLeftEncoderRise);
-  rightEncoder.rise(&onRightEncoderRise);
-  
-
+void setup() {  
   Serial.begin(115200);
 
   if (!BLE.begin()) {
@@ -48,6 +15,7 @@ void setup() {
   }
 
   RCC.Begin();
+  driver.Setup();
 
   Serial.println("BLE ready. Waiting for connection...");
 }
@@ -59,53 +27,23 @@ void loop() {
     while (RCC.Connected()) {
 
       if (RCC.JoystickUpdated) {
-        speedLeft = RCC.JoyStickY + RCC.JoyStickX;
-        speedRight = RCC.JoyStickY - RCC.JoyStickX;
+        float speedLeft = RCC.JoyStickY + RCC.JoyStickX;
+        float speedRight = RCC.JoyStickY - RCC.JoyStickX;
 
-        updateMotors();
+        driver.UpdateMotors(speedLeft, speedRight);
 
         Serial.println(String(RCC.JoyStickX) + "   " + String(RCC.JoyStickY) + "   " + String(speedLeft) + "   " + String(speedRight));
       }
 
       if (!RCC.Empty()) {
         String data = RCC.Dequeue();
+        if (data == "A") {
+          driver.StopMotors();
+        }
         Serial.println(data);
       }
     }
 
     Serial.println("Disconnected");
   }
-}
-
-void stopMotors() {
-  leftMotorPwm.write(0.0);
-  rightMotorPwm.write(0.0);
-}
-
-void updateMotors() {
-
-  if (speedLeft > 0) {
-    leftMotorDir = 1;
-  }
-  else {
-    leftMotorDir = 0;
-  }
-
-  if (speedRight > 0) {
-    rightMotorDir = 1;
-  }
-  else {
-    rightMotorDir = 0;
-  }
-
-  leftMotorPwm.write(abs(speedLeft));
-  rightMotorPwm.write(abs(speedRight));
-}
-
-void onLeftEncoderRise() {
-  leftEncoderCount++;
-}
-
-void onRightEncoderRise() {
-  rightEncoderCount++;
 }
